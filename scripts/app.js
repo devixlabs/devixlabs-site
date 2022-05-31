25b4fca3b2302c7a928b2bbf2a666a27eaca5a2a
Jeff Rade <jeffrade@gmail.com>
2022-05-30 20:45:44 -0500

    First design interation with Bootstrap5

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+// https://getbootstrap.com/docs/5.1/components/tooltips/#example-enable-tooltips-everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
